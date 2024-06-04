7ec723cdd3f9b6641b0ba8d4d1ff6c151b935bf1
Angel Almanza <almanza.angel245@gmail.com>
2024-06-04 12:12:04 -0700

    Added Study Plans Controller

---
+++ app/Http/Controllers/StudyPlanController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StudyPlanRequest;
+use Illuminate\Http\Request;
+use App\Models\StudyPlan;
+use Illuminate\Http\JsonResponse;
+
+class StudyPlanController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $studyPlans = StudyPlan::all();
+        return response()->json($studyPlans);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StudyPlanRequest $request): JsonResponse
+    {
+        $studyPlan = StudyPlan::create($request->all());
+        return response()->json($studyPlan, 201);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $studyPlan = StudyPlan::findOrFail($id);
+        return response()->json($studyPlan);
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(StudyPlanRequest $request, string $id): JsonResponse
+    {
+        $studyPlan = StudyPlan::findOrFail($id);
+        $studyPlan->update($request->all());
+        return response()->json($studyPlan);
+    }
+
+    public function destroy(string $id): JsonResponse
+    {
+        $studyPlan = StudyPlan::findOrFail($id);
+        $studyPlan->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\MajorController;
+use App\Http\Controllers\StudyPlanController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('classrooms', ClassroomController::class);
     Route::apiResource('teachers', TeacherController::class);
     Route::apiResource('majors', MajorController::class);
+    Route::apiResource('study-plans', StudyPlanController::class);
 });
